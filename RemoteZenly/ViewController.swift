42ea6dbf56f925ee68abe27bdb03b786e9ca7db6
Sergey Tursunov <fiveze@gmail.com>
2021-04-28 14:03:45 +0300

    Initial Commit

---
+++ RemoteZenly/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RemoteZenly
+//
+//  Created by Sergey Tursunov on 28.04.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
